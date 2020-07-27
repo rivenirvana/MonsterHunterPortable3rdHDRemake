9b6b53ddfad5dd3a5608fd65387d29be21ab0253
rivenirvana <acverain@up.edu.ph>
2020-07-27 13:22:55 +0800

    Add scripts for renewing textures in game directory

---
+++ scripts/renew_ini.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Renewing ini file..."
+echo "Copying new ini..."
+cp -f $HOME/dev/rivenirvana/MonsterHunterPortable3rdHDRemake/TEXTURES/NPJB40001/textures.ini $HOME/.config/ppsspp/PSP/TEXTURES/NPJB40001/
+echo "Finished copying."
+echo "Finished renewing."
+exit 0

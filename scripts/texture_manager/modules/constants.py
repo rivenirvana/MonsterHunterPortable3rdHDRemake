60f9d4c5720920ee6740b4088df06d77a49b9968
rivenirvana <acverain@up.edu.ph>
2020-07-28 23:26:02 +0800

    Transfer all scripts to https://github.com/rivenirvana/ppsspp-texture-manager

--- scripts/texture_manager/modules/constants.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# constants.py
-
-from os.path import expanduser
-
-CODE_MHP3RD = "NPJB40001"  # Monster Hunter Portable 3rd
-HOME_DIR = expanduser("~")
-GAME_DIR = f"{HOME_DIR}/.config/ppsspp/PSP/TEXTURES/{CODE_MHP3RD}/new/"
-HASH_DIR = f"{HOME_DIR}/dev/rivenirvana/MonsterHunterPortable3rdHDRemake/catalog/"
--- scripts/texture_manager/run.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# run.py
-
-from modules.constants import GAME_DIR, HASH_DIR
-from modules.watcher import TextureWatcher
-
-if __name__ == "__main__":
-    TextureWatcher(GAME_DIR, HASH_DIR).run()
